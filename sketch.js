cf7d13481b95243d4fae9d2cf2447166586a60a4
rbha917 <162877854+rbha917@users.noreply.github.com>
2024-03-11 16:12:52 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let counter = 0;
+let countInterval;
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  countInterval = setInterval(dots, 1000);
+}
+
+function mousePressed(){ 
+   setTimeout(function() {
+      icecream(random(0,200), random(0,300), 30, color(0, 255, 255));
+   }, 2000);
+}
+
+function icecream(x, y, size, colour){
+  noStroke()
+  fill(255, 255, 150);
+  triangle(x, y, x + 20, y + 40, x + 40, y);
+  fill(colour);
+  ellipseMode(CORNER);
+  ellipse(x, y - 35, 40, 41);
+}
+
+function dots(){
+  stroke('purple');
+  strokeWeight(10);
+  point(random(0, 100), random(0,200));
+  point(random(0,200), random(0,200));
+  point(random(0, 150), random(0,100));
+  point(random(0, 150), random(0, 300));
+  counter++;
+  if (counter >= 5){
+	clearInterval(countInterval);
+    counter = 0;
+    countInterval = setInterval(dots, 1000);
+    }
+}
+
